e7dfc29c23356c7590ce7810633416f52bf6bce3
Carol <acam5@aluno.ifal.edu.br>
2026-04-23 11:57:44 -0300

    Add files via upload

--- lib/tela_matematica.dart
+++ lib/tela_matematica.dart
@@ -23,11 +23,158 @@ class _telaMatematica extends State<TelaMatematica> {
                 'Matemática!',
                 style: GoogleFonts.chewy(
                   fontSize: 55,
-                  color: Colors.white,
+                  color: Colors.black,
                 ),
               ),
             ),
           ),
+
+        Row(
+            children: [
+              const SizedBox(width: 70),
+              ElevatedButton(
+                child: Text(
+                    '1',
+                    style: GoogleFonts.irishGrover(
+                      fontSize: 35,
+                    )
+                ),
+
+                onPressed: () {
+                  print("Botão clicado!");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFFB895),
+                  foregroundColor: Color(0xFFA77C57),
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+
+                  elevation: 8,
+                ),
+              )
+
+            ],
+          ),
+
+          Row(
+            children:[
+              const SizedBox(width: 240),
+              ElevatedButton(
+                child: Text(
+                    '2',
+                    style: GoogleFonts.irishGrover(
+                      fontSize: 35,
+                    )
+                ),
+
+                onPressed: () {
+                  print("Botão clicado!");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFFB895),
+                  foregroundColor: Color(0xFFA77C57),
+                  padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+
+                  elevation: 8,
+                ),
+              ),
+            ]
+          ),
+
+          Row(
+              children:[
+                const SizedBox(width: 75),
+                ElevatedButton(
+                  child: Text(
+                      '3',
+                      style: GoogleFonts.irishGrover(
+                        fontSize: 35,
+                      )
+                  ),
+
+                  onPressed: () {
+                    print("Botão clicado!");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFFB895),
+                    foregroundColor: Color(0xFFA77C57),
+                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+
+                    elevation: 8,
+                  ),
+                ),
+              ]
+          ),
+
+          Row(
+              children:[
+                const SizedBox(width: 240),
+                ElevatedButton(
+                  child: Text(
+                      '4',
+                      style: GoogleFonts.irishGrover(
+                        fontSize: 35,
+                      )
+                  ),
+
+                  onPressed: () {
+                    print("Botão clicado!");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFFB895),
+                    foregroundColor: Color(0xFFA77C57),
+                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+
+                    elevation: 8,
+                  ),
+                ),
+              ]
+          ),
+
+          Row(
+              children:[
+                const SizedBox(width: 75),
+                ElevatedButton(
+                  child: Text(
+                      '5',
+                      style: GoogleFonts.irishGrover(
+                        fontSize: 35,
+                      )
+                  ),
+
+                  onPressed: () {
+                    print("Botão clicado!");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFFB895),
+                    foregroundColor: Color(0xFFA77C57),
+                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+
+                    elevation: 8,
+                  ),
+                ),
+              ]
+          ),
+
         ],
       ),
 
