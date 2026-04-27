8e2912fa2e44d669dcd46e32e20d44d1c213d06e
nany-lima <mtsl2@aluno.ifal.edu.br>
2026-04-27 08:21:00 -0300

    Refactor TelaLinguagens UI and button interactions
    Updated the TelaLinguagens class to enhance UI elements and button functionalities.

--- lib/tela_linguagens.dart
+++ lib/tela_linguagens.dart
@@ -1,162 +1,181 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
 class TelaLinguagens extends StatelessWidget {
-  const TelaLinguagens({super.key});
+ const TelaLinguagens({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFFFE8FD),
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50.0),
-              child: Text(
-                'Linguagens!',
-                style: GoogleFonts.chewy(
-                  fontSize: 55,
-                  color: Colors.black,
 
-                ),
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              const SizedBox(width: 70),
-          ElevatedButton(
-            child: Text(
-              '1',
-              style: GoogleFonts.irishGrover(
-                fontSize: 35),
-            ),
-            onPressed: () {
-              print("Botão clicado!");
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFFF4C6E6),
-              foregroundColor: Color(0xFFC97EB2),
-              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     backgroundColor: const Color(0xFFFE9DE1),
+     body: Column(
+       children: [
+         Align(
+           alignment: Alignment.centerLeft,
+           child: Padding(
+             padding: const EdgeInsets.only(top: 14.0),
+             child: Container(
+               padding: const EdgeInsets.symmetric(
+                   horizontal: 30, vertical: 10),
+               alignment: Alignment.centerLeft, //"Level Up" a esquerda
 
-              elevation: 8,
-            ),
-          ),
 
-            ],
 
-          ),
 
-          Row(
-           children: [
-             const SizedBox(width: 230),
-           ElevatedButton(
-             child: Text(
-               '2',
-               style: GoogleFonts.irishGrover(
-                   fontSize: 35),
-             ),
-             onPressed: () {
-               print("Botão clicado!");
-             },
-             style: ElevatedButton.styleFrom(
-               backgroundColor: Color(0xFFF4C6E6),
-               foregroundColor: Color(0xFFC97EB2),
-               padding: EdgeInsets.symmetric(horizontal: 35, vertical: 21),
-               shape: RoundedRectangleBorder(
-                 borderRadius: BorderRadius.circular(20),
+               decoration: BoxDecoration(
+                 color: const Color(0xFFFD79D5),
+                 // Cor do fundo do retângulo
+                 borderRadius: BorderRadius.circular(5),
+                 // Deixa as pontas arredondadas
+                 border: Border.all(
+                   color: const Color(0xFFFDD6F1), // Contorno verde claro
+                   width: 3,
+                 ),
                ),
-
-               elevation: 8,
+               child: Text(
+                 'Level Up!',
+                 style: GoogleFonts.chewy(
+                   fontSize: 35,
+                   color: Colors.white,
+                 ),
+               ),
              ),
            ),
-           ]
-          ),
+         ),
+         const SizedBox (height: 40),
 
-          Row(
-              children: [
-                const SizedBox(width: 70),
-                ElevatedButton(
-                  child: Text(
-                    '3',
-                    style: GoogleFonts.irishGrover(
-                        fontSize: 35),
-                  ),
-                  onPressed: () {
-                    print("Botão clicado!");
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFFF4C6E6),
-                    foregroundColor: Color(0xFFC97EB2),
-                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 21),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
 
-                    elevation: 8,
-                  ),
-                ),
-              ]
-          ),
-          Row(
-              children: [
-                const SizedBox(width: 230),
-                ElevatedButton(
-                  child: Text(
-                    '4',
-                    style: GoogleFonts.irishGrover(
-                        fontSize: 35),
-                  ),
-                  onPressed: () {
-                    print("Botão clicado!");
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFFF4C6E6),
-                    foregroundColor: Color(0xFFC97EB2),
-                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 21),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
 
-                    elevation: 8,
-                  ),
-                ),
-              ]
-          ),
 
-          Row(
-              children: [
-                const SizedBox(width: 70),
-                ElevatedButton(
-                  child: Text(
-                    '5',
-                    style: GoogleFonts.irishGrover(
-                        fontSize: 35),
-                  ),
-                  onPressed: () {
-                    print("Botão clicado!");
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFFF4C6E6),
-                    foregroundColor: Color(0xFFC97EB2),
-                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 21),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+         //                   BOTÃO DE LINGUAGENS
 
-                    elevation: 8,
-                  ),
-                ),
-              ]
-          ),
 
-        ]
-      ),
-    );
-  }
+
+
+         Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 30.0),
+           child: SizedBox(
+             width: double.infinity, // Permite definir a largura da tela.
+             height: 100, // Largura do botão
+             child: ElevatedButton.icon(
+               onPressed: () {
+                 print("Clicou em Português");
+               },
+               icon: const Icon(Icons.history_edu, color: Colors.white),
+               label: const Text(
+                 'Português',
+                 style: TextStyle(fontSize: 25, color: Colors.white),
+               ),
+               style: ElevatedButton.styleFrom(
+                 backgroundColor: Color(0xFFFF59C8), // Cor do botão
+                 side: const BorderSide(
+                   color: Colors.white70,
+                   width: 3.0,
+                 ),
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(20),
+                 ),
+               ),
+             ),
+           ),
+         ),
+         const SizedBox(height: 20),
+         //BOTÃO INGLES
+         Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 30.0),
+           child: SizedBox(
+             width: double.infinity, // Permite definir a largura da tela.
+             height: 100, // Largura do botão
+             child: ElevatedButton.icon(
+               onPressed: () {
+                 print("Clicou em Iglês");
+               },
+               icon: const Icon(Icons.public, color: Colors.white),
+               label: const Text(
+                 'Inglês',
+                 style: TextStyle(fontSize: 25, color: Colors.white),
+               ),
+               style: ElevatedButton.styleFrom(
+                 backgroundColor: Color(0xFFFF59C8), // Cor do botão
+                 side: const BorderSide(
+                   color: Colors.white70,
+                   width: 3.0,
+                 ),
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(20),
+                 ),
+               ),
+             ),
+           ),
+         ),
+         const SizedBox(height: 20),
+
+
+
+
+         // BOTÃO ED>FISICA
+         Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 30.0),
+           child: SizedBox(
+             width: double.infinity, // Permite definir a largura da tela.
+             height: 100, // Largura do botão
+             child: ElevatedButton.icon(
+               onPressed: () {
+                 print("Clicou em Ed.Física");
+               },
+               icon: const Icon(Icons.directions_run, color: Colors.white),
+               label: const Text(
+                 'Educação Física',
+                 style: TextStyle(fontSize: 25, color: Colors.white),
+               ),
+               style: ElevatedButton.styleFrom(
+                 backgroundColor: Color(0xFFFF59C8), // Cor do botão
+                 side: const BorderSide(
+                   color: Colors.white70,
+                   width: 3.0,
+                 ),
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(20),
+                 ),
+               ),
+             ),
+           ),
+         ),
+         const SizedBox(height: 20),
+         //BOTÃO INGLES
+         Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 30.0),
+           child: SizedBox(
+             width: double.infinity, // Permite definir a largura da tela.
+             height: 100, // Largura do botão
+             child: ElevatedButton.icon(
+               onPressed: () {
+                 print("Clicou em Artes");
+               },
+               icon: const Icon(Icons.palette, color: Colors.white),
+               label: const Text(
+                 'Artes',
+                 style: TextStyle(fontSize: 25, color: Colors.white),
+               ),
+               style: ElevatedButton.styleFrom(
+                 backgroundColor: Color(0xFFFF59C8), // Cor do botão
+                 side: const BorderSide(
+                   color: Colors.white70,
+                   width: 3.0,
+                 ),
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(20),
+                 ),
+               ),
+             ),
+           ),
+         ),
+         const SizedBox(height: 20),
+       ],
+     ),
+   );
+ }
 }
+
