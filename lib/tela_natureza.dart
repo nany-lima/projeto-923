33941ef3cd877fd74b782b44f1e415c845cc060a
Rebeca Batista <rbs37@aluno.ifal.edu.br>
2026-04-28 15:01:03 -0300

    Update tela_natureza.dart

--- lib/tela_natureza.dart
+++ lib/tela_natureza.dart
@@ -1,36 +1,159 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-class TelaNatureza extends StatefulWidget {
-  const TelaNatureza({super.key});
-
-  @override
-  State<TelaNatureza> createState() => _telaNatureza();
-}
-
-class _telaNatureza extends State<TelaNatureza> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFD0FFEC),
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50.0),
-              child: Text(
-                'Ciências!',
-                style: GoogleFonts.chewy(
-                  fontSize: 55,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+import 'package:flutter/material.dart'; 
+import 'package:google_fonts/google_fonts.dart'; 
+class TelaNatureza extends StatefulWidget { 
+ const TelaNatureza({super.key}); 
+ 
+ @override 
+ State<TelaNatureza> createState() => _telaNatureza(); 
+} 
+ 
+class _telaNatureza extends State<TelaNatureza> { 
+ 
+ @override 
+ Widget build(BuildContext context) { 
+   return Scaffold( 
+     backgroundColor: const Color(0XFFCCFF90), 
+     body: Column( 
+       children: [ 
+         Align( 
+           alignment: Alignment.centerLeft, 
+           child: Padding( 
+             padding: const EdgeInsets.only(top: 16.0), 
+             child: Container( 
+               padding: const EdgeInsets.symmetric( 
+                   horizontal: 30, vertical: 10), 
+               alignment: Alignment.centerLeft,  
+ 
+               decoration: BoxDecoration( 
+                 color: const Color(0xFF388E3C), 
+                 borderRadius: BorderRadius.circular(5), 
+                 border: Border.all( 
+                   color: const Color(0xFF98FB98), 
+                   width: 3, 
+                 ), 
+               ), 
+               child: Text( 
+                 'Level Up!', 
+                 style: GoogleFonts.chewy( 
+                   fontSize: 35, 
+                   color: Colors.white, 
+                 ), 
+               ), 
+             ), 
+           ), 
+         ), 
+         const SizedBox (height: 40), 
+         const SizedBox(height: 20), 
+         Center( 
+           child: Text( 
+             'CIÊNCIAS!', 
+             style: GoogleFonts.poppins( 
+               fontSize: 38, 
+               fontWeight: FontWeight.bold, 
+               color: const Color(0xFF388E3C), 
+             ), 
+           ), 
+         ), 
+ 
+         const SizedBox(height: 40), 
+ 
+    Padding( 
+           padding: const EdgeInsets.symmetric(horizontal: 30.0), 
+           child: SizedBox( 
+             width: double.infinity, 
+             height: 100, 
+             child: ElevatedButton.icon( 
+               onPressed: () { 
+                 print("Clicou em Biologia"); 
+               }, 
+               icon: const Icon(Icons.eco, color: Colors.white, 
+size: 30), 
+               label: Text( 
+                 'Biologia', 
+                 style: TextStyle(fontSize: 25, color: 
+Colors.white), 
+ 
+               ), 
+               style: ElevatedButton.styleFrom( 
+                 backgroundColor: Color(0xFF388E3C), 
+                 side: const BorderSide( 
+                   color: Colors.white, 
+                   width: 2.0, 
+                 ), 
+                 shape: RoundedRectangleBorder( 
+                   borderRadius: BorderRadius.circular(20), 
+                 ), 
+               ), 
+             ), 
+           ), 
+         ), 
+         const SizedBox(height: 20), 
+ 
+         Padding( 
+           padding: const EdgeInsets.symmetric(horizontal: 30.0), 
+           child: SizedBox( 
+             width: double.infinity, 
+             height: 100,  
+             child: ElevatedButton.icon( 
+               onPressed: () { 
+                 print("Clicou em Física"); 
+               }, 
+               icon: const Icon(Icons.bolt, color: Colors.white, 
+size: 35), 
+               label: Text( 
+                 'Física', 
+                 style: TextStyle(fontSize: 25, color: 
+Colors.white), 
+ 
+               ), 
+               style: ElevatedButton.styleFrom( 
+                 backgroundColor: Color(0xFF388E3C), 
+                 side: const BorderSide( 
+                   color: Colors.white, 
+                   width: 2.0, 
+                 ), 
+                 shape: RoundedRectangleBorder( 
+                   borderRadius: BorderRadius.circular(20), 
+                 ), 
+               ), 
+             ), 
+           ), 
+         ), 
+         const SizedBox(height: 20), 
+ 
+         Padding( 
+           padding: const EdgeInsets.symmetric(horizontal: 30.0), 
+           child: SizedBox( 
+             width: double.infinity, 
+             height: 100, 
+             child: ElevatedButton.icon( 
+               onPressed: () { 
+                 print("Clicou em Química"); 
+               }, 
+               icon: const Icon(Icons.science, color: Colors.white, 
+size: 30), 
+               label: Text( 
+                 'Química', 
+                 style: TextStyle(fontSize: 25, color: 
+Colors.white), 
+ 
+               ), 
+               style: ElevatedButton.styleFrom( 
+                 backgroundColor: Color(0xFF388E3C), 
+                 side: const BorderSide( 
+                   color: Colors.white, 
+                   width: 2.0, 
+                 ), 
+                 shape: RoundedRectangleBorder( 
+                   borderRadius: BorderRadius.circular(20), 
+                 ), 
+               ), 
+             ), 
+           ), 
+         ), 
+ 
+ 
+       ], 
+     ), 
+   ); 
+ } 
